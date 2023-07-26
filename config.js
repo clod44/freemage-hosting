b8355a0b44f5e00f421e852c57844302bbcfbdef
clod44 <Rt_Sayochi@hotmail.com>
2023-07-26 15:35:52 -0700

    console error the missing mongo_uri

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ const ENABLE_HTTPS = (process.env.ENABLE_HTTPS || false) === 'true';
 const HTTP_PORT = parseInt(process.env.HTTP_PORT) || 2999;
 const HTTPS_PORT = parseInt(process.env.HTTPS_PORT) || 3000;
 //mongodb+srv:// ...
-const MONGO_URI = process.env.MONGO_URI || '- missing MONGO_URI in .env -';
+const MONGO_URI = process.env.MONGO_URI || false;
+if (!MONGO_URI) console.error("--- missing MONGO_URI in .env ---")
 const DB_NAME = process.env.DB_NAME || 'main';
 //this will contain a "dictionary" of random names and what file they will redirect to
 const COLLECTION_NAME = process.env.COLLECTION_NAME || 'image-mapping';
